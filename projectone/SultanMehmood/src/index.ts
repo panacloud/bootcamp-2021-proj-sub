d006474cf37d2b3e5cda81a5a8d30f42391da5c8
Sultan <m.sultan338@gmail.com>
2021-07-11 20:46:57 +0500

    Initial commit

--- projectone/SultanMehmood/src/index.ts
+++ projectone/SultanMehmood/src/index.ts
@@ -6,8 +6,11 @@ let list : TodoCollection = new TodoCollection();
 list.addTodo("Buy Mango");
 list.addTodo("Buy Meat");
 list.addTodo("Get Haircut");
+list.addTodo("Go for a walk");
+
 
 list.taskDone(2);
 
 list.printAll();
+
 
