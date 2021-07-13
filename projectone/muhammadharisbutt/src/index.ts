aacf8cc0f77a59648875920c8dea47ecce7b2707
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2021-07-14 01:50:57 +0500

    add TodoCollection

--- projectone/muhammadharisbutt/src/index.ts
+++ projectone/muhammadharisbutt/src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos :TodoItem[] = [
+    new TodoItem(1,"Buy Flowers"),
+    new TodoItem(2,"Get Shoes"),
+    new TodoItem(3,"COllect Tickets"),
+    new TodoItem(4,"Call Joe",true),
+]
+
+let collection : TodoCollection = new TodoCollection("Haris",todos)
+
 console.clear();
-console.log("Adam's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId:number  = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
