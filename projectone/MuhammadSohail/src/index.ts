bea82da303f3be01013e52f98b21745678947c3f
Muhammad Sohail <sohaildryabadi92@gmail.com>
2021-07-13 23:00:26 +0500

    Listing 1-29. Using a New Package inquirer

--- projectone/MuhammadSohail/src/index.ts
+++ projectone/MuhammadSohail/src/index.ts
@@ -1,16 +1,42 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
+
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
 console.clear();
 console.log(`${collection.userName}'s Todo List `
-+ `(${ collection.getItemCounts().incomplete } items to do)`);
+    + `(${collection.getItemCounts().incomplete} items to do)`);
 //console.log(`${collection.userName}'s Todo List`);
 //let newId: number = collection.addTodo("Go for run");
 //let todoItem: TodoItem = collection.getTodoById(newId);
 //todoItem.printDetails();
 //collection.addTodo(todoItem);
 //collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+//collection.getTodoItems(true).forEach(item => item.printDetails());
+
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+enum Commands {
+    Quit = "Quit"
+}
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+promptUser();
