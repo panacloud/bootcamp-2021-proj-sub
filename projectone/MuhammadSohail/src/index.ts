cd5f2888261badc3acaabfe342ebb5c3ffe042d2
Muhammad Sohail <sohaildryabadi92@gmail.com>
2021-07-13 23:40:37 +0500

    Listing 1-38. Completing Items, Marking Tasks

--- projectone/MuhammadSohail/src/index.ts
+++ projectone/MuhammadSohail/src/index.ts
@@ -26,7 +26,9 @@ function displayTodoList(): void {
 }
 enum Commands {
     Add = "Add New Task",
+    Complete = "Complete Task",
     Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit"
 }
 function promptAdd(): void {
@@ -38,6 +40,22 @@ function promptAdd(): void {
             }
             promptUser();
         })
+}
+
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item =>
+            ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as number[];
+        collection.getTodoItems(true).forEach(item =>
+            collection.markComplete(item.id,
+                completedTasks.find(id => id === item.id) != undefined));
+        promptUser();
+    })
 }
 
 function promptUser(): void {
@@ -57,6 +75,17 @@ function promptUser(): void {
                 break;
             case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
                 break;
         }
     })
