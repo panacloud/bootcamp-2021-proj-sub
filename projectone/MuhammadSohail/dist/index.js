f59e3f464ed170aff9be73eb21efe11c349f31c7
Muhammad Sohail <sohaildryabadi92@gmail.com>
2021-07-13 23:31:16 +0500

    Listing 1-36. Adding Tasks

--- projectone/MuhammadSohail/dist/index.js
+++ projectone/MuhammadSohail/dist/index.js
@@ -27,9 +27,20 @@ function displayTodoList() {
 }
 var Commands;
 (function (Commands) {
+    Commands["Add"] = "Add New Task";
     Commands["Toggle"] = "Show/Hide Completed";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then(answers => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
 function promptUser() {
     console.clear();
     displayTodoList();
@@ -44,6 +55,9 @@ function promptUser() {
             case Commands.Toggle:
                 showCompleted = !showCompleted;
                 promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
                 break;
         }
     });
