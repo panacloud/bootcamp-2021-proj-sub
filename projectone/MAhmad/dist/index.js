7a104450862da856d017327f28b7f31d48a7d9c5
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-07-14 11:46:04 +0500

    advanced todo app

--- projectone/MAhmad/dist/index.js
+++ projectone/MAhmad/dist/index.js
@@ -1,13 +1,98 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoList_1 = require("./todoList");
+const inquirer = require("inquirer");
+var Commands;
+(function (Commands) {
+    Commands["ShowAllTodos"] = "Show All Todos";
+    Commands["ShowDone"] = "Show Completed Todos";
+    Commands["ShowUndone"] = "Show Incomplete Todos";
+    Commands["ShowStats"] = "Show Stats of Todos";
+    // AddTodos = "Add Todos",
+    // DelTodos = "Delete Todos",
+    // DoneTodos = "Complete Todo",
+    Commands["DelDoneTodo"] = "Delete Completed Todos";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
 let allTodo = new todoList_1.TodoList();
-allTodo.addTodo('task');
-allTodo.addTodo('task2');
-allTodo.addTodo('task3');
-allTodo.completeTodo(3);
-allTodo.deleteTodo(2);
-console.log(allTodo.countTodo());
-// allTodo.getTodoWithStatus(true).forEach((todo: Todo) => todo.printTodo());
-// allTodo.deleteDoneTodo();
+function todoByStatus(Status) {
+    allTodo.getTodoWithStatus(Status).forEach((todo) => todo.printTodo());
+}
+;
 allTodo.printTodos();
+// function promptAdd(): void {
+//     inquirer.prompt({
+//         type: "input",
+//         name: "add",
+//         message: "Enter Todo:"
+//     }).then(answers => {
+//         if (answers["add"] !== "") {
+//             allTodo.addTodo(answers["add"]);
+//         }
+//         promptUser();
+//     });
+// }
+// function promptDel(): void {
+//     inquirer.prompt({
+//         type: "number",
+//         name: "del",
+//         message: "Enter TodoID to delete:"
+//     }).then(answers => {
+//         if (answers["del"] !== "") {
+//             allTodo.deleteTodo(answers["del"]);
+//         }
+//         promptUser();
+//     });
+// }
+// function promptDone(): void {
+//     inquirer.prompt({
+//         type: "number",
+//         name: "Done",
+//         message: "Enter TodoID to Complete:"
+//     }).then(answers => {
+//         if (answers["Done"] !== "") {
+//             allTodo.completeTodo(answers["Done"]);
+//         }
+//         promptUser();
+//     });
+// }
+function promptUser() {
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+        // badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.ShowAllTodos:
+                allTodo.printTodos();
+                promptUser();
+            case Commands.ShowDone:
+                todoByStatus(true);
+                promptUser();
+            case Commands.ShowUndone:
+                todoByStatus(false);
+                promptUser();
+            case Commands.ShowStats:
+                console.log(`Total: ${allTodo.countTodo().total}`);
+                console.log(`Completed: ${allTodo.countTodo().complete}`);
+                console.log(`Incomplete: ${allTodo.countTodo().incomplete}`);
+                promptUser();
+            // case Commands.AddTodos:
+            //     promptAdd();
+            // case Commands.DelTodos:
+            //     promptDel();
+            // case Commands.DoneTodos:
+            //     promptDone();
+            // case Commands.DelDoneTodo:
+            //     allTodo.deleteDoneTodo();
+            //     promptUser();
+            case Commands.Quit:
+                break;
+            default:
+                break;
+        }
+    });
+}
+promptUser();
--- projectone/MAhmad/src/index.ts
+++ projectone/MAhmad/src/index.ts
@@ -1,20 +1,103 @@
-import { Todo } from "./todo";
 import { TodoList } from "./todoList";
+import * as inquirer from 'inquirer';
+import { Todo } from "./todo";
+
+enum Commands {
+    ShowAllTodos = "Show All Todos",
+    ShowDone = "Show Completed Todos",
+    ShowUndone = "Show Incomplete Todos",
+    ShowStats = "Show Stats of Todos",
+    // AddTodos = "Add Todos",
+    // DelTodos = "Delete Todos",
+    // DoneTodos = "Complete Todo",
+    DelDoneTodo = "Delete Completed Todos",
+    Quit = "Quit"
+}
 
 let allTodo: TodoList = new TodoList();
 
-allTodo.addTodo('task');
-allTodo.addTodo('task2');
-allTodo.addTodo('task3');
-
-allTodo.completeTodo(3);
-
-allTodo.deleteTodo(2);
-
-console.log(allTodo.countTodo());
-
-// allTodo.getTodoWithStatus(true).forEach((todo: Todo) => todo.printTodo());
-
-// allTodo.deleteDoneTodo();
+function todoByStatus(Status: boolean): void {
+    allTodo.getTodoWithStatus(Status).forEach((todo: Todo) => todo.printTodo());
+};
 
 allTodo.printTodos();
+
+// function promptAdd(): void {
+//     inquirer.prompt({
+//         type: "input",
+//         name: "add",
+//         message: "Enter Todo:"
+//     }).then(answers => {
+//         if (answers["add"] !== "") {
+//             allTodo.addTodo(answers["add"]);
+//         }
+//         promptUser();
+//     });
+// }
+
+// function promptDel(): void {
+//     inquirer.prompt({
+//         type: "number",
+//         name: "del",
+//         message: "Enter TodoID to delete:"
+//     }).then(answers => {
+//         if (answers["del"] !== "") {
+//             allTodo.deleteTodo(answers["del"]);
+//         }
+//         promptUser();
+//     });
+// }
+
+// function promptDone(): void {
+//     inquirer.prompt({
+//         type: "number",
+//         name: "Done",
+//         message: "Enter TodoID to Complete:"
+//     }).then(answers => {
+//         if (answers["Done"] !== "") {
+//             allTodo.completeTodo(answers["Done"]);
+//         }
+//         promptUser();
+//     });
+// }
+
+function promptUser(): void {
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+        // badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.ShowAllTodos:
+                allTodo.printTodos();
+                promptUser();
+            case Commands.ShowDone:
+                todoByStatus(true);
+                promptUser();
+            case Commands.ShowUndone:
+                todoByStatus(false);
+                promptUser();
+            case Commands.ShowStats:
+                console.log(`Total: ${allTodo.countTodo().total}`);
+                console.log(`Completed: ${allTodo.countTodo().complete}`);
+                console.log(`Incomplete: ${allTodo.countTodo().incomplete}`);
+                promptUser();
+            // case Commands.AddTodos:
+            //     promptAdd();
+            // case Commands.DelTodos:
+            //     promptDel();
+            // case Commands.DoneTodos:
+            //     promptDone();
+            // case Commands.DelDoneTodo:
+            //     allTodo.deleteDoneTodo();
+            //     promptUser();
+            case Commands.Quit:
+                break;
+            default:
+                break;
+        }
+    })
+}
+promptUser();
