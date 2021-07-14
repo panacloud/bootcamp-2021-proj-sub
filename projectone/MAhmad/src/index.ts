9069bce4ad92fe99b31fdca4896129c9dcdd652b
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-07-14 12:02:17 +0500

    resolved error

--- projectone/MAhmad/src/index.ts
+++ projectone/MAhmad/src/index.ts
@@ -8,17 +8,27 @@ enum Commands {
     ShowUndone = "Show Incomplete Todos",
     ShowStats = "Show Stats of Todos",
     AddTodos = "Add Todos",
-    // DelTodos = "Delete Todos",
-    // DoneTodos = "Complete Todo",
+    DelTodos = "Delete Todos",
+    DoneTodos = "Complete Todo",
     DelDoneTodo = "Delete Completed Todos",
     Quit = "Quit"
 }
 
 let allTodo: TodoList = new TodoList();
 
+allTodo.addTodo('task');
+allTodo.addTodo('task2');
+allTodo.addTodo('task3');
+
+allTodo.completeTodo(3);
+
+allTodo.deleteTodo(2);
+
 function todoByStatus(Status: boolean): void {
     allTodo.getTodoWithStatus(Status).forEach((todo: Todo) => todo.printTodo());
-};
+}
+
+// allTodo.deleteDoneTodo();
 
 allTodo.printTodos();
 
@@ -35,31 +45,31 @@ function promptAdd(): void {
     });
 }
 
-// function promptDel(): void {
-//     inquirer.prompt({
-//         type: "number",
-//         name: "del",
-//         message: "Enter TodoID to delete:"
-//     }).then(answers => {
-//         if (answers["del"] !== "") {
-//             allTodo.deleteTodo(answers["del"]);
-//         }
-//         promptUser();
-//     });
-// }
+function promptDel(): void {
+    inquirer.prompt({
+        type: "number",
+        name: "del",
+        message: "Enter TodoID to delete:"
+    }).then(answers => {
+        if (answers["del"] !== "") {
+            allTodo.deleteTodo(answers["del"]);
+        }
+        promptUser();
+    });
+}
 
-// function promptDone(): void {
-//     inquirer.prompt({
-//         type: "number",
-//         name: "Done",
-//         message: "Enter TodoID to Complete:"
-//     }).then(answers => {
-//         if (answers["Done"] !== "") {
-//             allTodo.completeTodo(answers["Done"]);
-//         }
-//         promptUser();
-//     });
-// }
+function promptDone(): void {
+    inquirer.prompt({
+        type: "number",
+        name: "Done",
+        message: "Enter TodoID to Complete:"
+    }).then(answers => {
+        if (answers["Done"] !== "") {
+            allTodo.completeTodo(answers["Done"]);
+        }
+        promptUser();
+    });
+}
 
 function promptUser(): void {
     inquirer.prompt({
@@ -73,31 +83,39 @@ function promptUser(): void {
             case Commands.ShowAllTodos:
                 allTodo.printTodos();
                 promptUser();
+                break;
             case Commands.ShowDone:
                 todoByStatus(true);
                 promptUser();
+                break;
             case Commands.ShowUndone:
                 todoByStatus(false);
                 promptUser();
+                break;
             case Commands.ShowStats:
                 console.log(`Total: ${allTodo.countTodo().total}`);
                 console.log(`Completed: ${allTodo.countTodo().complete}`);
                 console.log(`Incomplete: ${allTodo.countTodo().incomplete}`);
                 promptUser();
+                break;
             case Commands.AddTodos:
                 promptAdd();
-            // case Commands.DelTodos:
-            //     promptDel();
-            // case Commands.DoneTodos:
-            //     promptDone();
-            // case Commands.DelDoneTodo:
-            //     allTodo.deleteDoneTodo();
-            //     promptUser();
-            case Commands.Quit:
                 break;
-            default:
+            case Commands.DelTodos:
+                promptDel();
+                break;
+            case Commands.DoneTodos:
+                promptDone();
+                break;
+            case Commands.DelDoneTodo:
+                allTodo.deleteDoneTodo();
+                promptUser();
+                break;
+            case Commands.Quit:
                 break;
         }
     })
 }
 promptUser();
+
+
