1e1feb270ce9eec5640958e27b632a1fb4451ad2
Muhammad Bilal <bilalarshad7x@gmail.com>
2021-07-17 12:42:30 +0500

    Adding Tasks using promptAdd()

--- projectone/MuhammadBilal/src/index.ts
+++ projectone/MuhammadBilal/src/index.ts
@@ -3,8 +3,7 @@ import { TodoCollection } from "./todoCollection";
 import * as inquirer from 'inquirer';
 let todos: TodoItem[] = [
 new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
-new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
-let collection: TodoCollection = new TodoCollection("Adam", todos);
+new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];let collection: TodoCollection = new TodoCollection("Adam", todos);
 let showCompleted = true;
 function displayTodoList(): void {
 console.log(`${collection.userName}'s Todo List `
@@ -12,8 +11,18 @@ console.log(`${collection.userName}'s Todo List `
 collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
 enum Commands {
+Add = "Add New Task",
 Toggle = "Show/Hide Completed",
 Quit = "Quit"
+}
+function promptAdd(): void {
+console.clear();
+inquirer.prompt({ type: "input", name: "add", message: "Enter task:"})
+.then(answers => {if (answers["add"] !== "") {
+collection.addTodo(answers["add"]);
+}
+promptUser();
+})
 }
 function promptUser(): void {
 console.clear();
@@ -23,12 +32,14 @@ type: "list",
 name: "command",
 message: "Choose option",
 choices: Object.values(Commands),
-//badProperty: true
 }).then(answers => {
 switch (answers["command"]) {
 case Commands.Toggle:
 showCompleted = !showCompleted;
-promptUser();25
+promptUser();
+break;
+case Commands.Add:
+promptAdd();
 break;
 }
 })
