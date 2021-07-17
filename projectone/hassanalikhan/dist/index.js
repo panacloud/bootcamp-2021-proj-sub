579dfd00a58660dd46b3f3390151466245f6d9ad
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-07-18 00:14:07 +0500

    Add inquirer.js to prompt user for input
          Install inquirer.js in the app using “npm i inquirer” a third-party package used for prompting to get user inputs. Working with typescript there is a need to define types too, doing it manually is a difficult task so install inquirer types using “npm install --save-dev @types/inquirer” which will provide us with types required for working with inquirer package. These installations will update “package.json” and create a “package-lock.json” file along with a “node_modules” folder. We have no intensions to keep track of the said folder thus add it to “.gitignore”.
          Update “index.ts” to import inquirer in the app and remove manually added methods on collection. Write a promptUser function which displays the collection when ever called and a list of options for the user to select. Add commands in the app for user to select. (add, toggle, complete, delete, quit).
          Compile and execute the code

--- projectone/hassanalikhan/dist/index.js
+++ projectone/hassanalikhan/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// imports
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 // List of Todos - Dummy data
 let todos = [
     new todoItem_1.TodoItem(1, "Go for run", true),
@@ -11,40 +13,93 @@ let todos = [
 ];
 // Collection of todos
 let collection = new todoCollection_1.TodoCollection("Hassan", todos);
-// Clear console
-console.clear();
-// Print Collection (Pre defined)
-console.log("*** Original Collection ***");
-console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
-collection.printAll();
-// add new todo to the collection
-// this will return a id of the newly added todo
-let newId1 = collection.addTodo("Watch Movie");
-let newId2 = collection.addTodo("Go to Sleep");
-// get todo's by Id
-let todoItem1 = collection.getTodoById(newId1);
-let todoItem2 = collection.getTodoById(newId2);
-// Print details of todo items fetched by id
-console.log("\n*** Todo's fetched by ID ***");
-todoItem1.printDetails();
-todoItem2.printDetails();
-// Print Updated Collection
-console.log("\n*** Updated Collection with new todo's ***");
-console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
-collection.printAll();
-// Update complete status of todos
-collection.markComplete(2, true);
-collection.markComplete(6, false);
-// Print Updated Collection
-console.log("\n*** Updated Collection with Complete Status Changed ***");
-console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
-collection.printAll();
-// fetch items based on complete status
-console.log("\n*** Get Todo Items based on complete status ***");
-console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
-collection.getTodoItems(true).forEach((item) => item.printDetails());
-// Remove completed Items
-collection.removeComplete();
-console.log("\n*** Updated Collection after completed tasks removed ***");
-console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
-collection.printAll();
+// For displaying completed tasks
+let showCompleted = true;
+// Function to display ToDo list, this function will be used with inquirer
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List \nTotal : ${collection.getItemCounts().total} \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${collection.getItemCounts().incomplete}`);
+    collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
+}
+// Commands to be displayed in the command prompt
+var Commands;
+(function (Commands) {
+    Commands["Add"] = "Add New Task";
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Complete"] = "Complete Task";
+    Commands["Purge"] = "Remove Completed Tasks";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+// Function to add new task from promp window
+function promptAdd() {
+    console.clear();
+    inquirer
+        .prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then((answers) => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
+// Function mark a task as completed/incomplete
+function promptComplete() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map((item) => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete,
+        })),
+    })
+        .then((answers) => {
+        let completedTasks = answers["complete"];
+        collection
+            .getTodoItems(true)
+            .forEach((item) => collection.markComplete(item.id, completedTasks.find((id) => id === item.id) != undefined));
+        promptUser();
+    });
+}
+// function to take input from user
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+    })
+        .then((answers) => {
+        switch (answers["command"]) {
+            // In case we select toggle b/w show and hide completed
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            // Case for adding new task
+            case Commands.Add:
+                promptAdd();
+                break;
+            // Case for marking a task as complete
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            // Case for removing completed tasks
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+        }
+    });
+}
+promptUser();
--- projectone/hassanalikhan/src/index.ts
+++ projectone/hassanalikhan/src/index.ts
@@ -1,6 +1,7 @@
 // imports
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 // List of Todos - Dummy data
 let todos: TodoItem[] = [
@@ -12,80 +13,107 @@ let todos: TodoItem[] = [
 
 // Collection of todos
 let collection: TodoCollection = new TodoCollection("Hassan", todos);
+// For displaying completed tasks
+let showCompleted: boolean = true;
 
-// Clear console
-console.clear();
+// Function to display ToDo list, this function will be used with inquirer
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List \nTotal : ${
+      collection.getItemCounts().total
+    } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
+      collection.getItemCounts().incomplete
+    }`
+  );
+  collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
+}
 
-// Print Collection (Pre defined)
-console.log("*** Original Collection ***");
-console.log(
-  `${collection.userName}'s Todo List \nTotal : ${
-    collection.getItemCounts().total
-  } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
-    collection.getItemCounts().incomplete
-  }`
-);
-collection.printAll();
+// Commands to be displayed in the command prompt
+enum Commands {
+  Add = "Add New Task",
+  Toggle = "Show/Hide Completed",
+  Complete = "Complete Task",
+  Purge = "Remove Completed Tasks",
+  Quit = "Quit",
+}
 
-// add new todo to the collection
-// this will return a id of the newly added todo
-let newId1: number = collection.addTodo("Watch Movie");
-let newId2: number = collection.addTodo("Go to Sleep");
+// Function to add new task from promp window
+function promptAdd(): void {
+  console.clear();
+  inquirer
+    .prompt({ type: "input", name: "add", message: "Enter task:" })
+    .then((answers) => {
+      if (answers["add"] !== "") {
+        collection.addTodo(answers["add"]);
+      }
+      promptUser();
+    });
+}
 
-// get todo's by Id
-let todoItem1: TodoItem = collection.getTodoById(newId1);
-let todoItem2: TodoItem = collection.getTodoById(newId2);
+// Function mark a task as completed/incomplete
+function promptComplete(): void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "checkbox",
+      name: "complete",
+      message: "Mark Tasks Complete",
+      choices: collection.getTodoItems(showCompleted).map((item) => ({
+        name: item.task,
+        value: item.id,
+        checked: item.complete,
+      })),
+    })
+    .then((answers) => {
+      let completedTasks = answers["complete"] as number[];
+      collection
+        .getTodoItems(true)
+        .forEach((item) =>
+          collection.markComplete(
+            item.id,
+            completedTasks.find((id) => id === item.id) != undefined
+          )
+        );
+      promptUser();
+    });
+}
 
-// Print details of todo items fetched by id
-console.log("\n*** Todo's fetched by ID ***");
-todoItem1.printDetails();
-todoItem2.printDetails();
-
-// Print Updated Collection
-console.log("\n*** Updated Collection with new todo's ***");
-console.log(
-  `${collection.userName}'s Todo List \nTotal : ${
-    collection.getItemCounts().total
-  } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
-    collection.getItemCounts().incomplete
-  }`
-);
-collection.printAll();
-
-// Update complete status of todos
-collection.markComplete(2, true);
-collection.markComplete(6, false);
-
-// Print Updated Collection
-console.log("\n*** Updated Collection with Complete Status Changed ***");
-console.log(
-  `${collection.userName}'s Todo List \nTotal : ${
-    collection.getItemCounts().total
-  } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
-    collection.getItemCounts().incomplete
-  }`
-);
-collection.printAll();
-
-// fetch items based on complete status
-console.log("\n*** Get Todo Items based on complete status ***");
-console.log(
-  `${collection.userName}'s Todo List \nTotal : ${
-    collection.getItemCounts().total
-  } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
-    collection.getItemCounts().incomplete
-  }`
-);
-collection.getTodoItems(true).forEach((item) => item.printDetails());
-
-// Remove completed Items
-collection.removeComplete();
-console.log("\n*** Updated Collection after completed tasks removed ***");
-console.log(
-  `${collection.userName}'s Todo List \nTotal : ${
-    collection.getItemCounts().total
-  } \tComplted : ${collection.getItemCounts().complete} \tIncomplete : ${
-    collection.getItemCounts().incomplete
-  }`
-);
-collection.printAll();
+// function to take input from user
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands),
+    })
+    .then((answers) => {
+      switch (answers["command"]) {
+        // In case we select toggle b/w show and hide completed
+        case Commands.Toggle:
+          showCompleted = !showCompleted;
+          promptUser();
+          break;
+        // Case for adding new task
+        case Commands.Add:
+          promptAdd();
+          break;
+        // Case for marking a task as complete
+        case Commands.Complete:
+          if (collection.getItemCounts().incomplete > 0) {
+            promptComplete();
+          } else {
+            promptUser();
+          }
+          break;
+        // Case for removing completed tasks
+        case Commands.Purge:
+          collection.removeComplete();
+          promptUser();
+          break;
+      }
+    });
+}
+promptUser();
