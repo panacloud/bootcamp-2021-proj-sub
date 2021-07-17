022676a74c33e2dc6a4d1d1ded1ab32df0c35397
Muhammad Bilal <bilalarshad7x@gmail.com>
2021-07-17 12:39:05 +0500

    Adding Commands in index.ts

--- projectone/MuhammadBilal/src/index.ts
+++ projectone/MuhammadBilal/src/index.ts
@@ -5,12 +5,14 @@ let todos: TodoItem[] = [
 new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
 new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 function displayTodoList(): void {
 console.log(`${collection.userName}'s Todo List `
 + `(${ collection.getItemCounts().incomplete } items to do)`);
-collection.getTodoItems(true).forEach(item => item.printDetails());
+collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
 enum Commands {
+Toggle = "Show/Hide Completed",
 Quit = "Quit"
 }
 function promptUser(): void {
@@ -20,11 +22,15 @@ inquirer.prompt({
 type: "list",
 name: "command",
 message: "Choose option",
-choices: Object.values(Commands)
-badProperty : true;
-}).then(answers => {if (answers["command"] !== Commands.Quit) {
-  promptUser();
-  }
-  })
-  }
-  promptUser();
+choices: Object.values(Commands),
+//badProperty: true
+}).then(answers => {
+switch (answers["command"]) {
+case Commands.Toggle:
+showCompleted = !showCompleted;
+promptUser();25
+break;
+}
+})
+}
+promptUser();
