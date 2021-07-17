c113438aaaa7b31ebc5cff0c1fcf6684aca838c0
Abdullah Bin Tahir <48406637+code-aesthete@users.noreply.github.com>
2021-07-17 15:42:45 +0500

    add complete application files

--- projectone/ziakhan/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const todoCollection_1 = require("./todoCollection");
-let list = new todoCollection_1.TodoCollection();
-list.addTodo("Buy Mango");
-list.addTodo("Buy Meat");
-list.addTodo("Get Haircut");
-list.taskDone(2);
-list.printAll();
