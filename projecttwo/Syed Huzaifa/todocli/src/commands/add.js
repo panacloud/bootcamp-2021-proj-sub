4e320020613fe9634a4991f0ad15e8c01249acf8
Syed Huzaifa <huzaifanau123@gmail.com>
2021-07-23 23:50:32 +0500

    Updated add .js

--- projecttwo/Syed Huzaifa/todocli/src/commands/add.js
+++ projecttwo/Syed Huzaifa/todocli/src/commands/add.js
@@ -1,3 +1,17 @@
 // add.js
 const {Command, flags) = require('@oclif/command')
 const {Todo} = require('../db')
+
+class AddCommand extends Command {
+    async run() {
+        const {
+            flags: {task},
+        } = this.parse(AddCommand)
+        const res = await Todo.push({
+            task,
+            id: Todo.value().length,
+            done: false,
+        }).write()
+        this.log(res)
+    }
+}
