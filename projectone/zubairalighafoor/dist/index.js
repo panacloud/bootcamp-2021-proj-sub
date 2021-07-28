49fc7fa0402060d37745bec2b1124bea91ad4d2e
Zubair Ali <55566847+zubairalighafoor@users.noreply.github.com>
2021-07-28 12:56:49 +0500

    v3 of index and todoCollection by zubair

--- projectone/zubairalighafoor/dist/index.js
+++ projectone/zubairalighafoor/dist/index.js
@@ -8,8 +8,6 @@ let todos = [
 ];
 let collection = new todoCollection_1.TodoCollection("Adam", todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
-todoItem.printDetails();
-//collection.addTodo(todoItem);
+console.log(`${collection.userName}'s Todo List `
+    + `(${collection.getItemCounts().incomplete} items to do)`);
+collection.getTodoItems(true).forEach(item => item.printDetails());
--- projectone/zubairalighafoor/src/index.ts
+++ projectone/zubairalighafoor/src/index.ts
@@ -5,8 +5,6 @@ let todos: TodoItem[] = [
  new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
-let newId: number = collection.addTodo("Go for run");
-let todoItem: TodoItem = collection.getTodoById(newId);
-todoItem.printDetails();
-//collection.addTodo(todoItem);
+console.log(`${collection.userName}'s Todo List `
+ + `(${ collection.getItemCounts().incomplete } items to do)`);
+collection.getTodoItems(true).forEach(item => item.printDetails());
