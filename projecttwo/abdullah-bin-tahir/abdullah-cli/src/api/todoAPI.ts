d26d190d66369bb2e2234602595964864c52f6b6
Abdullah Bin Tahir <48406637+code-aesthete@users.noreply.github.com>
2021-07-31 01:06:48 +0500

    initiate todoAPI

---
+++ projecttwo/abdullah-bin-tahir/abdullah-cli/src/api/todoAPI.ts
@@ -0,0 +1,48 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
+
+//? os.homedir()
+//* get path of the home directory for the current user
+//* On windows, It collects its value from an environment variable called USERPROFILE if defined.
+//* Otherwise, it returns the path to the profile directory for the current user.
+//* Example: C:\Users\hc
+
+//? this will be the json "database" having todos
+const todoFile = path.join(os.homedir(), "abdullah-cli", "todos.json");
+
+//* an interface defines the specification of an entity
+//* handels the *what* part, but not the *how* part
+interface Todo {
+  done: boolean;
+  todo: string;
+}
+
+class TodoAPI {
+  private todos: Todo[] = [];
+
+  constructor() {
+    //* JSON.parse: converts JSON to object
+    //* readFileSync: Synchronously reads the entire contents of a file.
+    this.todos = JSON.parse(fs.readFileSync(todoFile, { encoding: "utf-8" }));
+  }
+
+  private saveTodos() {
+    //* existsSync: Synchronously tests whether or not the given
+    //* path exists by checking with the file system.
+    if (!fs.existsSync(path.dirname(todoFile))) {
+      fs.mkdirSync(path.dirname(todoFile));
+    }
+    //? JS to JSON
+    const data = JSON.stringify(this.todos);
+    fs.writeFileSync(todoFile, data, { encoding: "utf-8" });
+  }
+
+  add(todo: string) {
+    const newTodo: Todo = { done: false, todo };
+    this.todos.push(newTodo);
+    this.saveTodos();
+  }
+
+  //todo
+}
