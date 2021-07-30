a6a29bdc74887654335ecebf1f102b2e20638431
Abdullah Bin Tahir <48406637+code-aesthete@users.noreply.github.com>
2021-07-31 00:41:05 +0500

    add Add Todo command

---
+++ projecttwo/abdullah-bin-tahir/abdullah-cli/src/commands/add.ts
@@ -0,0 +1,20 @@
+//* abdullah-cli add [todo] => todo is an arg, no flag is needed
+import { Command } from "@oclif/command";
+import chalk from "chalk";
+
+export default class Add extends Command {
+  static description = "Add a new Todo to the list";
+
+  static args = [{ name: "todo" }];
+
+  async run() {
+    const { args } = this.parse(Add);
+
+    const todo = args.todo;
+    if (todo) {
+      this.log(`[Success] New todo added: ${todo}`);
+    } else {
+      this.error("Please specify a new todo");
+    }
+  }
+}
