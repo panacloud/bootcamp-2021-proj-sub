476c81b10cd46508ceb7567dd1112272f41a2b77
Muhammad Zubair <zubimughal99@gmail.com>
2021-07-30 20:19:28 +0500

    My first Project

---
+++ projectone/Muhammad_Zubair/muhammads_todo/src/todoItem.ts
@@ -0,0 +1,10 @@
+export class TodoItem {
+    constructor(public id: number,public task: string, public complete: boolean = false) {
+        }
+        
+    printDetails() : void {
+    console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)": ""}`);
+        }
+    }
+
+    // This project is code by Nuhammad Zubair Mughal
