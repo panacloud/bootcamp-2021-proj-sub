4de8f0baa695d6e17c18397738ce96492f0d0c5f
Muhammad Bilal <bilalarshad7x@gmail.com>
2021-07-31 15:05:17 +0500

    Improve add.ts to update task status as 'done'

--- projecttwo/Muhammad Bilal/asgard/src/commands/add.ts
+++ projecttwo/Muhammad Bilal/asgard/src/commands/add.ts
@@ -1,4 +1,4 @@
-import {Command} from '@oclif/command'
+import {Command, flags} from '@oclif/command'
 import chalk from 'chalk'
 import todoAPI from '../api/todoAPI'
 
@@ -7,11 +7,19 @@ export default class Add extends Command {
 
   static args = [{name: 'todo'}]
 
+  static flags = {
+    done: flags.boolean({ char: 'd' })
+  }
+
   async run() {
-    const {args} = this.parse(Add)
+    const {args, flags} = this.parse(Add)
     const todo   = args.todo
     if (todo) {
-      todoAPI.add(todo)
+      if (flags.done) {
+        todoAPI.add(todo)
+      } else {
+        todoAPI.add(todo)
+      }
       this.log(`${chalk.green('[Success]')} Added new todo: ${todo}`)
     } else {
       this.error(chalk.red('please specify the new todo'))
