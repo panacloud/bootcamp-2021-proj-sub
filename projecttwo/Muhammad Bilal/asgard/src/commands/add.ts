69d20397ec4b316f15b5b6b823daf3ddd277a897
Muhammad Bilal <bilalarshad7x@gmail.com>
2021-07-31 14:35:59 +0500

    Add First Task named "New Todo"

--- projecttwo/Muhammad Bilal/asgard/src/commands/add.ts
+++ projecttwo/Muhammad Bilal/asgard/src/commands/add.ts
@@ -1,25 +1,17 @@
-import {Command, flags} from '@oclif/command'
+import {Command} from '@oclif/command'
 
 export default class Add extends Command {
-  static description = 'describe the command here'
+  static description = 'Add new todo to list'
 
-  static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static args = [{name: 'todo'}]
 
   async run() {
-    const {args, flags} = this.parse(Add)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from D:\\Typescript\\Bootcamp 2021\\bootcamp-2021-proj-sub\\projecttwo\\Muhammad Bilal\\asgard\\src\\commands\\add.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const {args} = this.parse(Add)
+    const todo   = args.todo
+    if (todo) {
+      this.log(`[Success] Added new todo: ${todo}`)
+    } else {
+      this.error('please specify the new todo')
     }
   }
 }
