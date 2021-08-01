45aa3a56de2c99a675d1616265b7bd03964031c4
mAsadSiddique <asadsiddiuque.a@gmail.com>
2021-08-01 16:09:53 +0500

    Handle Error's in show.ts file

--- projecttwo/consoletodo/src/commands/show.ts
+++ projecttwo/consoletodo/src/commands/show.ts
@@ -7,18 +7,20 @@ export default class Show extends Command {
   static description = `Shows existing tasks
   ...
   Shows all the tasks sorted by their ids
-  `
+  `;
 
   async run() {
     const res = await Todo.sortBy('id').value()
-    res.forEach(({id, task, done}) =>
-      this.log(
-        `
+    // this.log(res)
+    res.forEach(
+      ({id, task, done}: { id: any; task: string; done: boolean }) =>
+        this.log(
+          `
           ${chalk.magenta(id)} ${
   done ? chalk.green('DONE') : chalk.yellowBright('NOT DONE')
 } : ${task}
         `
-      )
+        )
     )
   }
 }
