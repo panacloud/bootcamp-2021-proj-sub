f7965930ae7961edc35eec6ae5aab6ca52fa3cfd
faizsarwar <faizsarwar44@gmail.com>
2021-08-03 03:30:47 +0500

    displaying items count

--- projectone/MuhammadFaizSarwar/dist/index.js
+++ projectone/MuhammadFaizSarwar/dist/index.js
@@ -1,21 +1,5 @@
 "use strict";
-// console.clear();
-// console.log("hello");
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { TodoItem } from "./todoItem";
-// import { TodoCollection } from "./todocollections";
-// let todos = [
-//     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
-//     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
-// let collection = new TodoCollection("Adam", todos);
-// console.clear();
-// console.log(`${collection.userName}'s Todo List`);
-// let newId = collection.addTodo("Go for run");
-// let todoItem = collection.getTodoById(newId);
-// // console.log(JSON.stringify(todoItem)); // shows last todo item in the list
-// todoItem.printDetails();
-// collection.addTodo(todoItem);  // error here fn accepts string not object
-//updating file
 const todoItem_1 = require("./todoItem");
 const todocollections_1 = require("./todocollections");
 let todos = [
@@ -24,6 +8,6 @@ let todos = [
 ];
 let collection = new todocollections_1.TodoCollection("Adam", todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
-collection.removeComplete(); // removing completed items
+console.log(`${collection.userName}'s Todo List `
+    + `(${collection.getItemCounts().incomplete} items to do)`);
 collection.getTodoItems(true).forEach(item => item.printDetails());
--- projectone/MuhammadFaizSarwar/src/index.ts
+++ projectone/MuhammadFaizSarwar/src/index.ts
@@ -1,32 +1,29 @@
-// console.clear();
-// console.log("hello");
-
-// import { TodoItem } from "./todoItem";
-// import { TodoCollection } from "./todocollections";
-
-// let todos = [
-//     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
-//     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
-
-// let collection = new TodoCollection("Adam", todos);
-// console.clear();
-// console.log(`${collection.userName}'s Todo List`);
-// let newId = collection.addTodo("Go for run");
-// let todoItem = collection.getTodoById(newId);
-// // console.log(JSON.stringify(todoItem)); // shows last todo item in the list
-// todoItem.printDetails();
-
-// collection.addTodo(todoItem);  // error here fn accepts string not object
-
-
-//updating file
 import { TodoItem } from "./todoItem";
-import { TodoCollection } from "./todocollections";
+import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
-console.clear();
-console.log(`${collection.userName}'s Todo List`);
-collection.removeComplete();                                                    // removing completed items
-collection.getTodoItems(true).forEach(item => item.printDetails());
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${ collection.getItemCounts().incomplete } items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+enum Commands {
+    Quit = "Quit"
+}
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+            type: "list",
+            name: "command",
+            message: "Choose option",
+            choices: Object.values(Commands)
+    }).then(answers => { if (answers["command"] !== Commands.Quit) {
+        promptUser();
+    }
+})
+}
+promptUser();
