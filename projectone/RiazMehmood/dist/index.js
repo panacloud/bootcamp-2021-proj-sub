dbcd6eefb6424a5ce0c83a041ee865abdfcee653
Riaz Mehmood <engr.riaz2010@gmail.com>
2021-08-12 21:55:32 +0500

    added Map, Remove, Count Method

--- projectone/RiazMehmood/dist/index.js
+++ projectone/RiazMehmood/dist/index.js
@@ -8,9 +8,12 @@ let todos = [
     new todoItem_1.TodoItem(3, "Collect Tickets"),
     new todoItem_1.TodoItem(4, "Call Joe", true),
 ];
-let collection = new todoCollection_1.TodoCollection("Adam", todos);
+let collection = new todoCollection_1.TodoCollection("Riaz", todos);
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
-let newId = collection.addTodo("Go for run");
-let todoItem = collection.getTodoById(newId);
-console.log(JSON.stringify(todoItem));
+// let newId: number = collection.addTodo("Go for run");
+// let todoItem: TodoItem = collection.getTodoById(newId);
+// todoItem.printDetails();
+// collection.addTodo(todoItem);
+collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
--- projectone/RiazMehmood/dist/todoCollection.js
+++ projectone/RiazMehmood/dist/todoCollection.js
@@ -7,22 +7,34 @@ class TodoCollection {
         this.userName = userName;
         this.todoItems = todoItems;
         this.nextId = 1;
+        this.itemMap = new Map();
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
     addTodo(task) {
         while (this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.todoItems.push(new todoItem_1.TodoItem(this.nextId, task));
+        this.itemMap.set(this.nextId, new todoItem_1.TodoItem(this.nextId, task));
         return this.nextId;
     }
     getTodoById(id) {
-        return this.todoItems.find((item) => item.id === id);
+        return this.itemMap.get(id);
+    }
+    getTodoItems(includeComplete) {
+        return [...this.itemMap.values()].filter(item => includeComplete || !item.complete);
     }
     markComplete(id, complete) {
         const todoItem = this.getTodoById(id);
         if (todoItem) {
             todoItem.complete = complete;
         }
+    }
+    removeComplete() {
+        this.itemMap.forEach(item => {
+            if (item.complete) {
+                this.itemMap.delete(item.id);
+            }
+        });
     }
 }
 exports.TodoCollection = TodoCollection;
