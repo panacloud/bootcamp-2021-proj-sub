bcbb3bdacdacb53bb156ee1e7b9d3ab22f6e2735
Riaz Mehmood <engr.riaz2010@gmail.com>
2021-08-13 14:48:00 +0500

    Installed inquirer and types.

--- projectone/RiazMehmood/dist/index.js
+++ projectone/RiazMehmood/dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todos = [
     new todoItem_1.TodoItem(1, "Buy Flowers"),
     new todoItem_1.TodoItem(2, "Get Shoes"),
@@ -9,11 +10,39 @@ let todos = [
     new todoItem_1.TodoItem(4, "Call Joe", true),
 ];
 let collection = new todoCollection_1.TodoCollection("Riaz", todos);
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List ` +
+        `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+    })
+        .then((answers) => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+console.log(`${collection.userName}'s Todo List` +
+    `(${collection.getItemCounts().incomplete} items to do)`);
+// console.log(`${collection.userName}'s Todo List`);
 // let newId: number = collection.addTodo("Go for run");
 // let todoItem: TodoItem = collection.getTodoById(newId);
 // todoItem.printDetails();
 // collection.addTodo(todoItem);
-collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+// collection.removeComplete();
+collection.getTodoItems(true).forEach((item) => item.printDetails());
--- projectone/RiazMehmood/src/index.ts
+++ projectone/RiazMehmood/src/index.ts
@@ -1,5 +1,7 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
+
 let todos: TodoItem[] = [
   new TodoItem(1, "Buy Flowers"),
   new TodoItem(2, "Get Shoes"),
@@ -7,11 +9,44 @@ let todos: TodoItem[] = [
   new TodoItem(4, "Call Joe", true),
 ];
 let collection: TodoCollection = new TodoCollection("Riaz", todos);
+
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List ` +
+      `(${collection.getItemCounts().incomplete} items to do)`
+  );
+  collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
+enum Commands {
+  Quit = "Quit",
+}
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands),
+    })
+    .then((answers) => {
+      if (answers["command"] !== Commands.Quit) {
+        promptUser();
+      }
+    });
+}
+promptUser();
+
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+console.log(
+  `${collection.userName}'s Todo List` +
+    `(${collection.getItemCounts().incomplete} items to do)`
+);
+// console.log(`${collection.userName}'s Todo List`);
 // let newId: number = collection.addTodo("Go for run");
 // let todoItem: TodoItem = collection.getTodoById(newId);
 // todoItem.printDetails();
 // collection.addTodo(todoItem);
-collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails())
+// collection.removeComplete();
+collection.getTodoItems(true).forEach((item) => item.printDetails());
