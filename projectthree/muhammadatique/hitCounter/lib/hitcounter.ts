36aa2e3e649a321cb5b560a295912f6c93e4ae07
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-08-16 21:22:08 +0500

    Add table and envoirment structur to lib/hitcouter

--- projectthree/muhammadatique/hitCounter/lib/hitcounter.ts
+++ projectthree/muhammadatique/hitCounter/lib/hitcounter.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+//-------------------------------import dynamo
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
@@ -7,9 +9,32 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends cdk.Construct {
+/** allows accessing the counter function */
+public readonly handler: lambda.Function;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    // TODO
+//-------------------- defined a DynamoDB table with path as the partition key.
+
+    const table = new dynamodb.Table(this, 'Hits', {
+        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+
+//------------We defined a Lambda function which is bound to the lambda/hitcounter.handler code.
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'hitcounter.handler',
+        code: lambda.Code.fromAsset('lambda'),
+
+//----------We defined a Lambda function which is bound to the lambda/hitcounter.handler code.
+
+        environment: {
+            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+            HITS_TABLE_NAME: table.tableName
+        }
+    });
   }
 }
