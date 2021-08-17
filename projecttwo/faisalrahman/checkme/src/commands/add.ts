bb83954720cde30b597be2870e5be2ff0c46f7f8
faisalrahman36 <faisalrahman36@hotmail.com>
2021-08-17 16:45:55 +0500

    file path creation in todoAPI

--- projecttwo/faisalrahman/checkme/src/commands/add.ts
+++ projecttwo/faisalrahman/checkme/src/commands/add.ts
@@ -1,5 +1,6 @@
 import {Command} from '@oclif/command'
 import * as chalk from 'chalk'
+import todoAPI from '../api/todoAPI'
 
 export default class Add extends Command {
   static description = 'Add new todo to list'
@@ -10,6 +11,7 @@ export default class Add extends Command {
     const {args} = this.parse(Add)
     const todo   = args.todo
     if (todo) {
+      todoAPI.add(todo)
       this.log(`${chalk.green('[Success]')} Added new todo: ${todo}`)
     } else {
       this.error(chalk.red('please specify the new todo'))
