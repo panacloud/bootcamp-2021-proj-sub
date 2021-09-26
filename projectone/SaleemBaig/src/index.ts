3ee01c393d4c6557a51f2fc02449dc190a2ad380
saleembaig <gojalisaleem1@gmail.com>
2021-09-26 23:30:05 +0500

    Displaying Item Counts in the index.ts

--- projectone/SaleemBaig/src/index.ts
+++ projectone/SaleemBaig/src/index.ts
@@ -8,8 +8,9 @@ let todos: TodoItem[] = [
 ];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
+console.log(
+  `${collection.userName}'s Todo List ` +
+    `(${collection.getItemCounts().incomplete} items to do)`
+);
 
-//collection.addTodo(todoItem);
-collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
