57fcc5362037d10695513c4e79dc5c1107a134a4
Waleed Tariq <dev.ratech@outlook.com>
2021-10-17 00:41:25 +0500

    implement database

--- projectone/waleedtariq/src/index.ts
+++ projectone/waleedtariq/src/index.ts
@@ -1,13 +1,13 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 import * as inquirer from 'inquirer';
-
+import { JsonTodoCollection } from "./jsonTodoCollection";
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 
-let collection: TodoCollection = new TodoCollection("Waleed", todos);
+let collection: TodoCollection = new JsonTodoCollection("Waleed", todos);
 let showCompleted = true;
 
 function displayTodoList(): void {
